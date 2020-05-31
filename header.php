8dfb9b161712394467bd652d61562520b148190b
Matthew McKenzie <mojomckenzie@knights.ucf.edu>
2020-05-31 16:05:51 -0400

    V1 Update

--- header.php
+++ header.php
@@ -13,62 +13,19 @@
     <meta charset="utf-8">
     <title>MatchUp</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/MatchUp.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link href='https://fonts.googleapis.com/css?family=Actor' rel='stylesheet'>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/MatchUp.css">
+    <link rel="stylesheet" href="css/normalize.css">
   </head>
-  <body> 
-    <header class="row">
-          <div class="headLogo col-sm-2">
-            <img src="images/DIG3147_LOGO.svg" style="width: 100px;" alt="LOGO">
-          </div>
-          <div class="headNav col-sm-7">
-            <nav>
-              <ul>
-                <li><a class="navLink" href="index.php">Home</a></li>
-                <li><a class="navLink" href="contact.php">Contact Us</a></li>
-                <li><a class="navLink" href="index.php">Explore</a></li>
-                <li><a class="navLink" href="index.php">Connect</a></li>
-              </ul>
-            </nav>
-          </div>
-          <div class="headRegWrapper col-sm-3">
-            <div class="headReg">
-              <?php if ($_SESSION['loggedIn']){ ?>
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
-                  Delete Account
-                </button>
-
-                <!-- The Modal -->
-                <div class="modal" id="myModal">
-                  <div class="modal-dialog">
-                    <div class="modal-content">
-
-                      <!-- Modal Header -->
-                      <div class="modal-header">
-                        <h4 class="modal-title">Are you sure you want to delete your account?</h4>
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                      </div>
-
-                      <!-- Modal footer -->
-                      <div class="modal-footer">
-                        <a href="delete.php"><button type="button" class="btn btn-danger" >Yes, Im Sure</button></a>
-                      </div>
-
-                    </div>
-                  </div>
-                </div>
-                <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="POST">
-                  <input type="submit" name="logout" value="Log Out">
-                </form>
-              <?php } else { ?>
-              <a class="login" href="login.php">Log In</a>
-              <a class="signin btn btn-primary" href="register.php">Join Now</a>
-              <?php } ?>
-            </div>
-          </div>
-        </header>
+  <body class="container-fluid"> 
+    <header class="navbar navbar-expand-sm header">
+      <div class="headLogo">
+        <img src="img/DIG3147_LOGO.svg" style="width: 100px;" alt="LOGO">
+      </div> 
+    </header>
--- index.php
+++ index.php
@@ -1,73 +1,81 @@
 <?php 
-    include 'header.php';
+   include 'header.php';
 ?>
 
+<!-- USE ATTRIBUTION for App WireFrames Kit if use
 
-    <div class="heroPicWrapper container-fluid">
+<strong>Author</strong>:<a href="https://365psd.com/vector/app-wireframes-kit-56423" 
+title="App Wireframes Kit" target="_blank">App Wireframes Kit</a> from <a href="https://365psd.com" 
+target="_blank">365PSD.com</a>
+
+-->
+
+    <section>
+
+      <div class="pt-5">
+        <div><p>Why start with a ...</p><p class="animate">When you can start with a ...</p></div>
+      </div>
       
-      <div class="heroTextWrapper row">
-        <div class="heroText col">
-          <h5 class="">MATCH WITH THE WORLD</h5>
-          <h3 class="">THE LEADER IN CASUAL MATCHMAKING</h2>
-          <p>Stand out online with a professional website, <br>
-             online store, or portfolio. With Squarespace, <br>
-              you can turn any idea into a reality.</p>
-          <button class="btn btn-primary" type="button" name="button">Join Now!</button>
-        </div>
-        <div class="heroImg col">
+      <div class="animateWrapper w-100 h-100 d-flex justify-content-center">
+        <div class="centerAnimate d-flex justify-content-center">
+
+          <div class="IMessageWrapper">
+            <div class="textAnimateX">
+              <div class="textAnimateY">
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 138 61.11"><defs><style>.cls-1{fill:#46aad9;}.cls-2{font-size:29.2px;fill:#fdfefe;font-family:Helvetica;}</style></defs><path class="cls-1" d="M0,60.75c7-6.37,5.83-30.37,5.83-30.37h0C5.83,13.67,17.46,0,31.66,0h80.51C126.38,0,138,13.67,138,30.38h0c0,16.7-11.62,30.37-25.83,30.37H31.66a23.14,23.14,0,0,1-14.9-5.6S6,62.83,0,60.75Z"/><text class="cls-2" transform="translate(45.29 39.13)">Hey</text></svg>
+              </div>
+            </div>
+          </div>
+
+          <div class="picTransformWrapper">
+            <div class="newPicAnimation">
+              <img src="img/CoupleAtRestaurant.jpg" alt="Couple at Restuarant">
+              <div class="textAnimateTwo">
+                <svg data-name="Layer 1" style="width: 40px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 138 61.11"><defs><style>.cls-1{fill:#46aad9;}.cls-2{font-size:29.2px;fill:#fdfefe;font-family:Helvetica;}</style></defs><path class="cls-1" d="M0,60.75c7-6.37,5.83-30.37,5.83-30.37h0C5.83,13.67,17.46,0,31.66,0h80.51C126.38,0,138,13.67,138,30.38h0c0,16.7-11.62,30.37-25.83,30.37H31.66a23.14,23.14,0,0,1-14.9-5.6S6,62.83,0,60.75Z"/><text class="cls-2" transform="translate(45.29 39.13)">Hey</text></svg>
+              </div>
+            </div>
+          </div>
+          
+          <div class="registerCardWrapper mw-100">
+            <div class="card">
+              <div class="cardHead">
+                <h1>Get the link and Learn More</h1>
+              </div>
+              <div class="content mt-5 d-flex justify-content-center">
+                <form action="">
+                  <div class="form-group">
+                    <input type="phone" class="form-control text-center" name="phone" placeholder="Phone Number">
+                    <input type="submit" class="form-control w-50 mx-auto mt-2">
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
 
         </div>
       </div>
-    </div>
-    <section class="container-fluid">
-      <div class="row featuresWrapper">
-        <div class="col-3">
-          <h2>Choose a SQUAD for any Occasion</h2>
-        </div>
-        <div class="col-9">
-          <!--canvas id="canvas" width="500" height="500"></canvas-->
-          <!--script rel="javascript" src="js/MatchUp.js"></script-->
-          <img src="" alt="Picture of Group of Pictures">
-        </div>
-      </div>
-      <div class="row featuresWrapper">
-        <div class="col">
-          <h2>Biggest Concert of The Year or Small Movie Night</h2>
-        </div>
-        <div class="col">
-          <img src="" alt="Picture of Group of Pictures">
-        </div>
-      </div>
-      <div class="row featuresWrapper">
-        <div class="col">
-          <h2>New Untitled Feature</h2>
-          <p>Find the best locations to meetup</p>
-          <button type="button" name="button">Learn more</button>
-        </div>
-        <div class="col">
-          <img src="" alt="Picture of Group of Pictures">
-        </div>
-      </div>
-      <div class="row testimonyWrapper">
-        <div class="col">
-          <div class="testimony">
-            <img src="" alt="Profile Picture">
-            <p>This app is great, I use it all the time to meet new people!</p>
-          </div>
-          <div class="testimony">
-            <img src="" alt="Profile Picture">
-            <p>Whenever I visit a new place, it helps to get company quickly.</p>
-          </div>
-          <div class="testimony">
-            <img src="" alt="Profile Picture">
-            <p>I love this app, it's great!</p>
-          </div>
-        </div>
-      </div>
 
+ <!--
+      <div class="heroPicWrapper d-flex align-content-center row">
+        <div class="heroTextWrapper col">
+          <div class="heroText col">
+            <h5 class="">MATCH WITH THE WORLD</h5>
+            <h3 class="">THE LEADER IN CASUAL MATCHMAKING</h2>
+            <p>Stand out online with a professional website, <br>
+               online store, or portfolio. With Squarespace, <br>
+                you can turn any idea into a reality.</p>
+            <button class="btn btn-primary" type="button" name="button">Join Now!</button>
+          </div>
+        </div>
+        <div class="heroPhoneWrapper col">
+          <div class="heroPhoneMockup">
+            <img src="img/iphonemockup5.jpg" alt="Iphone showing MatchUp UI">
+          </div>
+        </div>
+      </div>
+-->
     </section>
-    <footer>
-
-    </footer>
-  </body>
-</html>
+  
+<?php 
+  include 'footer.php';
+?>
